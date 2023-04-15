26dee7d84ca1894574805a5a708af07660ab01dd
Name <email@example.ru>
2023-04-15 17:46:49 +0700

    first commit

---
+++ Task-2/script.js
@@ -0,0 +1,17 @@
+// Задание 2
+
+const btn = document.querySelector('.btn_cool');
+let myWidth;
+  let myHeight;
+
+
+btn.addEventListener('click', () =>
+{
+  myWidth = window.screen.width;
+  myHeight = window.screen.height;
+  alert(`Параметры вашего экрана: 
+  Ширина - ${myWidth} px, 
+  Высота - ${myHeight} px.`);
+
+  
+})
